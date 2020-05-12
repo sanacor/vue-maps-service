37639499c3e0ce3828633e3b3745ea9da4633bef
강산하 <ncsana@gmail.com>
2020-05-12 22:23:44 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Map></Map>
+    <v-bottom-navigation v-model="bottomNav">
+      <v-btn value="recent">
+        <span>Recent</span>
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+
+      <v-btn value="favorites">
+        <span>Favorites</span>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn value="nearby" bottomNav>
+        <span>Nearby</span>
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Map from "./components/Map.vue";
+// import BottomNavi from './components/BottomNavi.vue'
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,
+    Map
+    // BottomNavi
+  },
+  data: () => ({
+    bottomNav: "recent"
+  })
+};
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/Map.vue
@@ -0,0 +1,35 @@
+<template>
+  <vue-daum-map
+      :appKey="appKey"
+      :center.sync="center"
+      :level.sync="level"
+      :mapTypeId="mapTypeId"
+      :libraries="libraries"
+      @load="onLoad"
+      style="width:500px;height:400px;"/>
+</template>
+
+<script>
+import VueDaumMap from 'vue-daum-map'
+
+export default {
+  name: "Map",
+  components: {
+    VueDaumMap
+  },
+  data: () => ({
+    appKey: "b17c77211acfdb44a6e6d6a91310cd44", // 테스트용 appkey
+    center: { lat: 37.350152, lng: 127.117604 }, // 지도의 중심 좌표
+    level: 3, // 지도의 레벨(확대, 축소 정도),
+    mapTypeId: VueDaumMap.MapTypeId.NORMAL, // 맵 타입
+    libraries: [], // 추가로 불러올 라이브러리
+    map: null // 지도 객체. 지도가 로드되면 할당됨.
+  }),
+  methods: {
+    // 지도가 로드 완료되면 load 이벤트 발생
+    onLoad(map) {
+      this.map = map;
+    }
+  }
+};
+</script>

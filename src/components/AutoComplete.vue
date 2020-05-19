2917c13bee514d043fe1de4663545d364fe915ab
강산하 <ncsana@gmail.com>
2020-05-19 22:13:38 +0900

    AutoComplete 컴포넌트 추가

---
+++ src/components/AutoComplete.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-autocomplete
+        v-model="model"
+        :items="items"
+        :loading="isLoading"
+        :search-input.sync="search"
+        chips
+        clearable
+        hide-details
+        hide-selected
+        item-text="name"
+        item-value="symbol"
+        label="와인, 와인샵"
+        solo
+		prepend-inner-icon="mdi-magnify"
+      >
+  </v-autocomplete>
+</template>
+
+<script>
+export default {
+	name: "AutoComplete",
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,5 +1,6 @@
 <template>
   <v-card class="d-flex pa-2" outlined tile>
+    <AutoComplete></AutoComplete>
     <div id="dmap">
       <div id="map" style="width:100%; height:800px;"></div>
     </div>
@@ -7,8 +8,13 @@
 </template>
 
 <script>
+import AutoComplete from './AutoComplete.vue'
+
 export default {
   name: "Map",
+  components: {
+    AutoComplete
+  },
   data() {
     return {};
   },
@@ -19,10 +25,13 @@ export default {
       level: 4 //지도의 레벨(확대, 축소 정도)
     };
     new kakao.maps.Map(container, mapOptions);
-    axios.get('https://z6zh3ycr0d.execute-api.ap-northeast-2.amazonaws.com/dev/endpoint')
-      .then(function (response) {
+    axios
+      .get(
+        "https://z6zh3ycr0d.execute-api.ap-northeast-2.amazonaws.com/dev/endpoint"
+      )
+      .then(function(response) {
         console.log(response.data);
-      })
+      });
   }
 };
 </script>
